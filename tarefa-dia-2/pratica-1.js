44ff1e416eb73af7f10bb889c7084dcea3155dc6
Andre Rabelo <andrerabelom@gmail.com>
2022-08-23 22:56:34 -0300

    mais uma atualizacao

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -19,3 +19,6 @@ console.log("Eu tenho "+idade+ " anos de idade")
 for (i in lista) {
 console.log(lista[i])
 }
+
+
+console.log("Olá," + nome + ". Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para" + lugarDosSonhos +  ". Confirma pra gente alguns dados? A sua idade é" + idade + ", você é " + profissao + " e o seu email é" + email + "? Ficamos no aguardo. Muito obrigado e boa viagem! :D");
