97739e6a3bd0c7d961d04171f027bbe9b40053b0
Andre Rabelo <andrerabelom@gmail.com>
2022-08-23 22:45:51 -0300

    atualizacao da pratica1

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -3,3 +3,19 @@ let email = "andrerabelom@gmail.com"
 let profissao = "Analista de Processos"
 let idade = 26
 let lugarDosSonhos = "Suiça"
+
+
+let lista = [nome, email, profissao, idade, lugarDosSonhos]
+
+console.log("Meu nome é " + nome);
+
+console.log("Meu e-mail é: " + email);
+
+console.log("Minha profissão é: "+profissao)
+
+console.log("Eu tenho "+idade+ " anos de idade")
+
+
+for (i in lista) {
+console.log(lista[i])
+}
