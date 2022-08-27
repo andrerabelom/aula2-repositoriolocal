b6e78521513d5ce2bbdde2a9edabd516a0322981
Andre Rabelo <andrerabelom@gmail.com>
2022-08-27 17:33:04 -0300

    Dia 3 tarefa 2

---
+++ tarefa-dia-3/pratica-2.js
@@ -0,0 +1,12 @@
+let numerotabuada = 8;
+
+let i=1
+
+console.log("Tabuada de " +numerotabuada)
+
+
+while (i<= numerotabuada) {
+    console.log(`${i} x ${numerotabuada} = ${i*numerotabuada}`);
+    i++
+}
+
